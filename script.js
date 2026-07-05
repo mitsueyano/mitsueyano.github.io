cc6fc9660223f79586b1c622197896a5641336cd
Luiza Mitsue Yano <106928510+mitsueyano@users.noreply.github.com>
2026-07-05 18:00:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+function loadProjects() {
+  const timeline = document.querySelector('.timeline');
+  if (!timeline) return;
+  timeline.innerHTML = '';
+
+  fetch('projects.json')
+    .then(response => {
+      if (!response.ok) {
+        console.log('Erro ao carregar o arquivo json');
+      }
+      return response.json();
+    })  
+    .then(data => {
+      data.forEach(project => {
+
+        const item = document.createElement('div');
+        item.classList.add('item');
+
+        item.innerHTML = `
+          <span class="year">${project.year}</span>
+          <div class="card">
+            <h3>${project.title}</h3>
+            <p class="inst">${project.institution}</p>
+            <p>${project.description}</p>
+            <img src="./img/${project.image}" alt="Projeto ${project.title}" />
+            <a href="./projects/${project.link}/">Ver detalhes</a>
+          </div>
+        `;
+        
+        timeline.appendChild(item);
+      });
+    })
+    .catch(error => console.error('Erro:', error));
+}
+
+function getYear() {
+  const yearSpan = document.getElementById('year');
+  if (yearSpan) {
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+  }
+}
+
+// Menu mobile
+const menuToggle = document.querySelector('.menuToggle');
+const navUl = document.querySelector('nav ul');
+
+if (menuToggle && navUl) {
+    menuToggle.addEventListener('click', () => {
+        navUl.classList.toggle('open');
+        menuToggle.classList.toggle('open');
+    });
+
+    document.querySelectorAll('nav a').forEach(link => {
+        link.addEventListener('click', () => {
+            navUl.classList.remove('open');
+            menuToggle.classList.remove('open');
+        });
+    });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadProjects();
+  getYear();
+});
+
